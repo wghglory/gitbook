66a655962002ddba19b4536813391c12095d2229
Guanghui Wang <wghglory89@gmail.com>
2017-09-28 17:05:42 +0800

    目录生成初版

--- code.js
+++ code.js
@@ -1,120 +1,75 @@
-// var fs = require('fs'),
-//   stdin = process.stdin,
-//   stdout = process.stdout;
-// var stats = [];
+const fs = require('fs');
+const path = require('path');
+const sep = require('path').sep;
+const root_path = process.argv[2] || __dirname; // /Users/derek/guanghui.notebook
+const w_file = 'README.md';
+const projectName = 'guanghui.notebook';
 
-// fs.readdir(process.cwd(), function(err, files) {
-//   console.log(' ');
+//为了省事，这里采用了字符串拼接的方式，最优的方式应该用buffer来做字符拼接。
+let symbol = function(fpath) {
+  let s = '';
+  for (let i = 2; i < fpath.length; i++) {
+    s += '  ';
+  }
+  return s;
+};
 
-//   if (!files.length) {
-//     return console.log(' \033[31m No files to show!\033[39m\n');
-//   }
-
-//   function file(i) {
-//     var filename = files[i];
-
-//     fs.stat(__dirname + '/' + filename, function(err, stat) {
-//       stats[i] = stat;
-//       if (stat.isDirectory()) {
-//         console.log(' ' + i + ' \033[36m' + filename + '/\033[39m');
-//       } else {
-//         console.log(' ' + i + ' \033[90m' + filename + '\033[39m');
-//       }
-
-//       i++;
-
-//       if (i == files.length) {
-//         read();
-//       } else {
-//         file(i);
-//       }
-//     });
-//   }
-
-//   function read() {
-//     console.log(' ');
-//     stdout.write(' \033[33mEnter your choice : \033[39m');
-//     stdin.resume();
-//     stdin.setEncoding('utf8');
-//     stdin.on('data', option);
-//   }
-
-//   function option(data) {
-//     var filename = files[Number(data)];
-//     if (!files[Number(data)]) {
-//       stdout.write(' \033[mEnter your choice : \033[39m');
-//     } else if (stats[Number(data)].isDirectory()) {
-//       fs.readdir(__dirname + '/' + filename, function(err, files) {
-//         console.log(' ');
-//         console.log(' (' + files.length + 'files)');
-//         files.forEach(function(file) {
-//           console.log(' - ' + file);
-//         });
-//         console.log(' ');
-//       });
-//     } else {
-//       stdin.pause();
-//       fs.readFile(__dirname + '/' + filename, 'utf8', function(err, data) {
-//         console.log(' ');
-//         console.log('\033[90m' + data.replace(/(.*) /g, ' $1') + '\033[39m');
-//       });
-//     }
-//   }
-
-//   file(0);
-// });
-
-var fs = require('fs');
-var path = require('path');
-var root_path = process.argv[2] || __dirname;
-var w_file = 'README.md';
-
-function getAllFiles(root) {
-  var res = [];
-  var files = fs.readdirSync(root).sort((a, b) => {
+function getFilesAndFolders(root) {
+  let res = [];
+  let fileAndFolders = fs.readdirSync(root).sort((a, b) => {
     return a.localeCompare(b, undefined /* Ignore language */, { sensitivity: 'base' });
   });
 
-  console.log(files);
-  // files.sort(function(a, b) {
+  // 根据时间来排序
+  // fileAndFolders.sort(function(a, b) {
   //   return fs.statSync(dir + a).mtime.getTime() - fs.statSync(dir + b).mtime.getTime();
   // });
 
-  files.forEach(function(file) {
-    var pathname = root + '/' + file;
-    if ((path.extname(file) === '.md' && file !== 'README.md') || (path.extname(file) === '' && file !== '.DS_Store')) {
-      var stat = fs.lstatSync(pathname);
-      var one = `${pathname.replace(`${root_path}/`, '- [')}](${pathname.replace(`${root_path}/`, '')})`;
-      if (!stat.isDirectory()) {
-        res.push(one);
+  fileAndFolders.forEach(function(f) {
+    let pathname = root + '/' + f; // /Users/derek/Library/guanghui.notebook/移动端/viewport.md
+    let pathname2 = pathname.replace(/.+\/(guanghui.notebook.+)/, '$1'); // guanghui.notebook/移动端/viewport.md
+
+    // 不为 README.md 的 markdown 文件；排除 node_modules folder；
+    if (
+      (path.extname(f) === '.md' && f !== 'README.md' && f !== 'SUMMARY.md') ||
+      (path.extname(f) === '' && !f.startsWith('.') && !f.startsWith('_') && f !== 'node_modules' && f !== 'assets')
+    ) {
+      let stat = fs.lstatSync(pathname);
+
+      if (stat.isDirectory()) {
+        let fpath = pathname2.split(sep);
+        // console.info(symbol(fpath) + fpath[fpath.length - 1]);
+        let folderName = pathname.replace(/.+guanghui.notebook\/(.+)/, '$1');
+        res.push(symbol(fpath) + `* [${folderName}](/${folderName}/README.md)`);
+        createReadMe4Folders(pathname, '# TOC\r\n\r\n' + res.join('\n'));
+        res = res.concat(getFilesAndFolders(pathname));
       } else {
-        res = res.concat(getAllFiles(pathname));
+        let fpath = pathname2.split(sep);
+        // console.info(symbol(fpath) + fpath[fpath.length - 1]);
+        let bracketName = pathname.replace(/.+\/(.+)\.md/, '$1'); // viewport
+        let parenthesisName = pathname.replace(root_path, '').slice(1); // 移动端/viewport.md
+        res.push(symbol(fpath) + `- [${bracketName}](${parenthesisName})`); // - [viewport](移动端/viewport.md)
       }
     }
   });
   return res;
 }
-var w_content = getAllFiles(root_path).join('\n');
+const toc = '# SUMMARY\r\n\r\n' + getFilesAndFolders(root_path).join('\n');
 
-fs.open('README.md', 'w', function(err, fd) {
-  fs.write(fd, w_content, 0, 'utf8', function(e) {
+// write TOC for summary.md
+fs.open('SUMMARY.md', 'w', function(err, fd) {
+  fs.write(fd, toc, 0, 'utf8', function(e) {
     if (e) throw e;
     fs.closeSync(fd);
   });
 });
 
-// fs.readFile(root_path + w_file, function(err, data) {
-//   if (err && err.errno == 33) {
-//     fs.open(w_file, 'w', 0666, function(e, fd) {
-//       if (e) throw e;
-//       fs.write(fd, w_content, 0, 'utf8', function(e) {
-//         if (e) throw e;
-//         fs.closeSync(fd);
-//       });
-//     });
-//   } else {
-//     fs.writeFile(root_path + w_file, w_content, function(e) {
-//       if (e) throw e;
-//     });
-//   }
-// });
+// write TOC for every folder
+function createReadMe4Folders(path, toc) {
+  fs.open(path + 'README.md', 'w', function(err, fd) {
+    fs.write(fd, toc, 0, 'utf8', function(e) {
+      if (e) throw e;
+      fs.closeSync(fd);
+    });
+  });
+}
