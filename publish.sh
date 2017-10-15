8ec47a1219be8f4bbbeab6b2d11d3cabfc2c95fc
Guanghui Wang <wghglory89@gmail.com>
2017-10-15 22:21:18 +0800

    update publish script

--- publish.sh
+++ publish.sh
@@ -4,14 +4,12 @@ git rm --cached -r . &&   # 删除不需要的文件
 git clean -df &&
 rm -rf *~ &&
 cp -r _book/* . &&  # 复制 _book 下的内容到分支中
-echo "*~" > .gitignore  # 添加 gitignore
-echo "_book" >> .gitignore
-echo ".DS_Store" >> .gitignore
-echo ".editorconfig" >> .gitignore
-echo "publish.sh" >> .gitignore
-echo "package.json" >> .gitignore
-echo "generateTOC.js" >> .gitignore
-echo "generateTOC.js" >> .gitignore
+echo "*~" > .gitignore && # 添加 gitignore
+echo ".DS_Store" >> .gitignore &&
+echo ".editorconfig" >> .gitignore &&
+echo "publish.sh" >> .gitignore &&
+echo "package.json" >> .gitignore &&
+echo "generateTOC.js" >> .gitignore &&
 git add . &&
 echo published on $(date +%Y-%m-%d_%H:%M:%S) | git commit -F -  &&  # https://unix.stackexchange.com/questions/366407/git-commit-using-stdout-from-bash
 git push -u origin gh-pages
