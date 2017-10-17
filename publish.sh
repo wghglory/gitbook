d6df788a6dc4bc5080fe27d110bb7fbf0e29c74b
Guanghui Wang <wghglory89@gmail.com>
2017-10-18 00:05:43 +0800

    🆙

--- publish.sh
+++ publish.sh
@@ -1,11 +1,13 @@
 # 参考 http://www.chengweiyang.cn/gitbook/github-pages/README.html
+# 后续更新用 publish
 git checkout gh-pages &&
 git rm --cached -r . &&   # 删除不需要的文件
 git clean -df &&
 rm -rf *~ &&
 cp -r _book/* . &&  # 复制 _book 下的内容到分支中
 echo "*~" > .gitignore && # 添加 gitignore
-echo "_book" > .gitignore && # 添加 gitignore
+echo "_book" >> .gitignore && # 添加 gitignore
+echo "node_modules" >> .gitignore && # 添加 gitignore
 echo ".DS_Store" >> .gitignore &&
 echo ".editorconfig" >> .gitignore &&
 echo "publish.sh" >> .gitignore &&
